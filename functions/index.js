0a102c64e4ffb551fd9bdd2040a8bd68287d50b8
Psalms91-50-23 <khothach@gmail.com>
2021-11-14 17:09:20 +1300

    working fine so far, gone live

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,10 @@
 const functions = require("firebase-functions");
 const express = require("express")
 const cors = require("cors");
-const stripe = require('stripe')('sk_test_51JrBJFCeBeR4kdgiouDqGImQDA4t8dK0OZmkDv4akDpD4XKbl6dQgGTZ5JED7GAKletkDgnpu6dNcNkUoB7e3iW700n6BvgevJ')
+require('dotenv').config()
+
+
+const stripe = require('stripe')(process.env.REACT_APP_API)
 
 //API
 
@@ -13,7 +16,8 @@ const app = express()
 //Middlewares
 // app.use(cors({origin: true}))
 //origin is the local host you are allowing to cross realm
-app.use(cors({origin: "http://localhost:3000"}))
+// app.use(cors({origin: "http://localhost:3000"}))
+app.use(cors({origin: "https://khos-17ce8.web.app"}))
 app.use(express.json())
 
 
@@ -22,8 +26,6 @@ app.get("/",(req,res) => {
     res.status(200).send("hello world")
 })
 
-// var cors = require('cors');    
-// app.use(cors({credentials: true, origin: 'http://localhost:5000'}));
 
 app.post("/payment/create", async (req,res) => {
 
@@ -31,7 +33,7 @@ app.post("/payment/create", async (req,res) => {
     //in payments component   url: `/payments/create?total=${totalPrice}`
     const total = Number(req.query.total)
 
-    console.log("Payment Request Recieved amount is >>> ", total)
+    // console.log("Payment Request Recieved amount is >>> ", total)
 
     //the amount cant be decimals, it is x100 in payment.js from client side, it receives in cents
     const paymentIntent = await stripe.paymentIntents.create({
@@ -39,7 +41,7 @@ app.post("/payment/create", async (req,res) => {
         currency: "nzd",
     })
 
-    console.log("paymentIntent >>>>>> ",paymentIntent)
+    // console.log("paymentIntent >>>>>> ",paymentIntent)
     // OK - created
     res.status(201).send({
         clientSecret: paymentIntent.client_secret,
@@ -53,11 +55,3 @@ app.post("/payment/create", async (req,res) => {
 //got its api name as exports.api
 exports.api = functions.https.onRequest(app)
 
-
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
