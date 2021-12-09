5acc78bb7b2971966ff11be82a03a922480ce556
Psalms91-50-23 <khothach@gmail.com>
2021-12-09 22:50:26 +1300

    fixed header geolocation, 'deliver to' appeared even though location access is denied, now shows only if access is granted

--- src/components/Header.js
+++ src/components/Header.js
@@ -54,7 +54,7 @@ const Header = () => {
                     alt=""
                 />
             </NavLink>
-            { geoLocation ? 
+            { location?
                ( 
                    
                 <div className="header__geoLocation">
